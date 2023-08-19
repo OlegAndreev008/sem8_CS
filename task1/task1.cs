24f3bd965fa472934379fe90fc137042fbf9e19b
OlegAndreev <andreev008@mail.ru>
2023-08-19 21:05:31 +0300

    task1 ready

--- task1/task1.cs
+++ task1/task1.cs
@@ -1,2 +1,53 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int[,] massiv = new int[3, 4];
+void Fill(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+void Change(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1) - 1; j++)
+        {
+            if (arr[i, j] < arr[i, j + 1])
+            {
+                int temp = arr[i, j];
+                arr[i, j] = arr[i, j + 1];
+                arr[i, j + 1] = temp;
+                i = j = 0;
+            }
+        }
+    }
+}
+Fill(massiv);
+Print(massiv);
+Console.WriteLine();
+Change(massiv);
+Print(massiv);
