48496857468536d26c3db431ef11dca4f7acc3b6
OlegAndreev <andreev008@mail.ru>
2023-08-24 23:17:44 +0300

    add task5

--- task5/task5.cs
+++ task5/task5.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("vvedite A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("vvedite B: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+int Degree(int a, int b)
+{
+    if (b == 0) return 1;
+    else return a * Degree(a, b - 1);
+}
+int help = Degree(num1, num2);
+Console.WriteLine(help);
