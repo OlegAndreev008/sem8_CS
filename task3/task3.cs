374f6e81f80b8985acb69b81bd6b1a1868f5174d
OlegAndreev <andreev008@mail.ru>
2023-08-24 20:27:37 +0300

    add tasks 3 4

--- task3/task3.cs
+++ task3/task3.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+// Например, даны 2 матрицы:
+// 2 4 | 3 4
+// 3 2 | 3 3
+// Результирующая матрица будет:
+// 18 20
+// 15 18
+int[,] massivA = new int[3, 3];
+int[,] massivB = new int[3, 3];
+void Fill(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+void Print(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+int[,] Scalar(int[,] arr1, int[,] arr2)
+{
+    int[,] arr = new int[arr1.GetLength(0), arr2.GetLength(1)];
+    for (int i = 0; i < arr1.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr2.GetLength(1); j++)
+        {
+            arr[i, j] = 
+        }
+    }
+}
+Fill(massivA);
+Fill(massivB);
+Print(massivA);
+Console.WriteLine();
+Print(massivB);
